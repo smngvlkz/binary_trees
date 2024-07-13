a88c2ad9ddfc45112dae22c7e9138202957831ff
TheCodingPantsula <asmdigital14@gmail.com>
2024-07-13 14:51:29 +0200

    New node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -12,9 +12,11 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
-	new_node = malloc(sizeof(binary_tree_t));
+	new_node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
+	{
 		return (NULL);
+	}
 
 	new_node->n = value;
 	new_node->parent = parent;
